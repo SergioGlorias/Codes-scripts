e12842681c85081fd957064c5e6977536d418293
Sérgio Glórias <9739913+SergioGlorias@users.noreply.github.com>
2024-07-27 10:35:51 +0000

    list

--- only-titles.js
+++ only-titles.js
@@ -3,11 +3,8 @@ const hostname = "https://bot.slaycer.top"
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 const fetchFIDE = async (page) => {
-    return await fetch(`${hostname}/api/players?std%5Bgte%5D=2100&sort_by=standard&order_by=desc&page=${page}&page_size=100`)
-        .then(res => {
-            console.log(res.status)
-            return res.json()
-        })
+    return await fetch(`${hostname}/api/players?sort_by=standard&order_by=desc&page=${page}&page_size=100`)
+        .then(res => res.json())
         .then(json => json)
 }
 
@@ -27,7 +24,31 @@ const fetchFIDE = async (page) => {
         }
     }
 
-    let masters = await players.filter(p => Object.hasOwn(p, "otherTitles"))
+    let otherTitles =   players.filter(p => Object.hasOwn(p, "otherTitles"))
+    let IA          =   otherTitles.filter(p => p.otherTitles.includes("IA")).length
+    let FA          =   otherTitles.filter(p => p.otherTitles.includes("FA")).length
+    let NA          =   otherTitles.filter(p => p.otherTitles.includes("NA")).length
+    let IO          =   otherTitles.filter(p => p.otherTitles.includes("IO")).length
+    let FT          =   otherTitles.filter(p => p.otherTitles.includes("FT")).length
+    let FI          =   otherTitles.filter(p => p.otherTitles.includes("FI")).length
+    let FST         =   otherTitles.filter(p => p.otherTitles.includes("FST")).length
+    let DI          =   otherTitles.filter(p => p.otherTitles.includes("DI")).length
+    let NI          =   otherTitles.filter(p => p.otherTitles.includes("NI")).length
+    let SI          =   otherTitles.filter(p => p.otherTitles.includes("SI")).length
+    let LSI         =   otherTitles.filter(p => p.otherTitles.includes("LST")).length
+    let otherMaster =   otherTitles.filter(p => Object.hasOwn(p, "title") || Object.hasOwn(p, "womenTitle")).length
 
-    console.log(`[ IA, FA, NA, IO, FT, FI, FST, DI, NI, SI, LSI ]: ${masters.length}`)
+    console.info(`[ IA, FA, NA, IO, FT, FI, FST, DI, NI, SI, LSI ]: ${otherTitles.length}`)
+    console.info(`IA: ${IA}`)
+    console.info(`FA: ${FA}`)
+    console.info(`NA: ${NA}`)
+    console.info(`IO: ${IO}`)
+    console.info(`FT: ${FT}`)
+    console.info(`FI: ${FI}`)
+    console.info(`FST: ${FST}`)
+    console.info(`DI: ${DI}`)
+    console.info(`NI: ${NI}`)
+    console.info(`SI: ${SI}`)
+    console.info(`LSI: ${LSI}`)
+    console.info(`With Master Title: ${otherMaster} (Without: ${otherTitles.length - otherMaster})`)
 })()
